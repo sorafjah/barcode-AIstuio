1ed7460f67a1f584eb45bef964ecf3a24e055373
そらふや <tofya17@gmail.com>
2026-02-15 12:37:19 +0900

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: './', // Allow app to run in subdirectories
+  build: {
+    outDir: 'dist',
+  }
 });
